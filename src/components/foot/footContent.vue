996abcad42e0da7d5f5f504e15d17e751c1e6992
hzdgood <hzd_163@sina.com>
2022-03-01 16:17:02 +0800

    updata code

--- src/components/foot/footContent.vue
+++ src/components/foot/footContent.vue
@@ -2,12 +2,9 @@
   <div>
     <div class="footDiv">
       <a-menu mode="horizontal">
-        <a-menu-item key="Info" @click="clickInfo()">信息</a-menu-item>
+        <a-menu-item key="Info" @click="clickInfo()">项目信息</a-menu-item>
         <a-menu-item key="collect" @click="clickCollect()">收款</a-menu-item>
         <a-menu-item key="scheme" @click="clickScheme()">方案</a-menu-item>
-        <a-menu-item key="workSteet" @click="clickSteet()">工单</a-menu-item>
-        <!-- <a-menu-item key="follow" @click="clickFollow()">跟进</a-menu-item>
-        <a-menu-item key="survey" @click="clickSurvey()">工勘</a-menu-item> -->
       </a-menu>
     </div>
     <div v-if="infoShow">
@@ -16,17 +13,8 @@
     <div v-if="collectShow">
       <collectEdit></collectEdit>
     </div>
-    <div v-if="followShow">
-      <followEdit></followEdit>
-    </div>
-    <div v-if="surveyShow">
-      <surveyEdit></surveyEdit>
-    </div>
     <div v-if="schemeShow">
       <schemeEdit></schemeEdit>
-    </div>
-    <div v-if="steetShow">
-      <workSteetEdit></workSteetEdit>
     </div>
   </div>
 </template>
@@ -36,10 +24,7 @@ import { Component, Vue } from 'vue-property-decorator'
 import { Icon, Menu } from 'ant-design-vue'
 import infoEdit from '@/components/foot/footEdit/infoEdit.vue'
 import collectEdit from '@/components/foot/footEdit/collectEdit.vue'
-import followEdit from '@/components/foot/footEdit/followEdit.vue'
-import surveyEdit from '@/components/foot/footEdit/surveyEdit.vue'
 import schemeEdit from '@/components/foot/footEdit/schemeEdit.vue'
-import workSteetEdit from '@/components/foot/footEdit/workSteetEdit.vue'
 @Component({
   name: 'foot',
   components: {
@@ -48,20 +33,14 @@ import workSteetEdit from '@/components/foot/footEdit/workSteetEdit.vue'
     'a-icon': Icon,
     infoEdit: infoEdit,
     collectEdit: collectEdit,
-    followEdit: followEdit,
-    surveyEdit: surveyEdit,
-    schemeEdit: schemeEdit,
-    workSteetEdit: workSteetEdit
+    schemeEdit: schemeEdit
   }
 })
 export default class Home extends Vue {
   // async mounted () {}
   infoShow: any = false;
   collectShow: any = false;
-  followShow: any = false;
-  surveyShow: any = false;
   schemeShow: any = false;
-  steetShow: any = false;
 
   // 回调 infoEdit
   reload () {
@@ -85,35 +64,11 @@ export default class Home extends Vue {
     }
   }
 
-  clickFollow () {
-    if (this.followShow) {
-      this.followShow = false
-    } else {
-      this.followShow = true
-    }
-  }
-
-  clickSurvey () {
-    if (this.surveyShow) {
-      this.surveyShow = false
-    } else {
-      this.surveyShow = true
-    }
-  }
-
   clickScheme () {
     if (this.schemeShow) {
       this.schemeShow = false
     } else {
       this.schemeShow = true
-    }
-  }
-
-  clickSteet () {
-    if (this.steetShow) {
-      this.steetShow = false
-    } else {
-      this.steetShow = true
     }
   }
 }
--- src/components/nav/Nav.vue
+++ src/components/nav/Nav.vue
@@ -1,44 +1,31 @@
 <template>
   <a-menu v-model="current" mode="horizontal">
-    <a-menu-item key="Home">
+    <a-menu-item key="项目信息">
       <router-link to="/">
-        <!-- <a-icon type="home" /> -->
         信息
       </router-link>
     </a-menu-item>
-
-    <a-menu-item key="工单">
-      <router-link to="/workSheet">
-        <!-- <a-icon type="user" /> -->
-        工单
-      </router-link>
-    </a-menu-item>
-    <a-menu-item key="发货">
-      <router-link to="/sendOut">
-        <!-- <a-icon type="usergroup-add" /> -->
-        发货
-      </router-link>
-    </a-menu-item>
-    <a-menu-item key="方案">
-      <router-link to="/scheme">
-        <!-- <a-icon type="usergroup-add" /> -->
-        方案
-      </router-link>
-    </a-menu-item>
-
-    <!-- <a-menu-item key="跟进">
+    <a-menu-item key="跟进">
       <router-link to="/followUp">
         <a-icon type="setting" />
         跟进
       </router-link>
     </a-menu-item>
-    <a-menu-item key="工勘">
-      <router-link to="/enginInvestiga">
-        <a-icon type="usergroup-add" />
-        工勘
+    <a-menu-item key="发货">
+      <router-link to="/sendOut">
+        发货
       </router-link>
-    </a-menu-item> -->
-
+    </a-menu-item>
+    <a-menu-item key="方案">
+      <router-link to="/scheme">
+        方案
+      </router-link>
+    </a-menu-item>
+    <a-menu-item key="工单">
+      <router-link to="/workSheet">
+        工单
+      </router-link>
+    </a-menu-item>
   </a-menu>
 </template>
 
@@ -55,10 +42,9 @@ import { Icon, Menu } from 'ant-design-vue'
   }
 })
 export default class Nav extends Vue {
-  current: string[] = [this.$route.name as string]
+  current: string[] = [this.$route.name as string];
 }
 </script>
 
 <style scoped>
-
 </style>
