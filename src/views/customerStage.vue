4133c6ca969adc55e3dbdebac78b4057fde49eb7
hzdgood <hzd_163@sina.com>
2022-05-31 20:11:41 +0800

    update code

--- src/views/customerStage.vue
+++ src/views/customerStage.vue
@@ -1,26 +1,24 @@
 <template>
   <div class="stageDiv">
     <div class="stageButton">
-      <!-- <button
-        v-for="item in customStage"
-        :key="item.id"
-        :id="item.id"
-        :name="item.field"
-        @click="onclick(item)"
-      >
-        {{ item.value }}
-      </button> -->
       <span>
-        <img src="../img/x1.png" />
+        <img src="../img/x1.png" width="25%" />
       </span>
       <span>
-        <img src="../img/state-1.png" />
+        <img id="img" src="../img/state-1.png" width="73%" v-show="stageImg1" />
+        <img src="../img/state-2.png" width="73%" v-show="stageImg2" />
+        <img src="../img/state-3.png" width="73%" v-show="stageImg3" />
+        <img src="../img/state-4.png" width="73%" v-show="stageImg4" />
+        <img src="../img/state-5.png" width="73%" v-show="stageImg5" />
+        <img src="../img/state-6.png" width="73%" v-show="stageImg6" />
+        <img src="../img/state-7.png" width="73%" v-show="stageImg7" />
+        <img src="../img/state-8.png" width="73%" v-show="stageImg8" />
       </span>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 import { SearchInfo, updateTable } from '@/config/interFace'
 import { table, field } from '@/config/config'
 @Component({})
@@ -31,10 +29,27 @@ export default class Home extends Vue {
   taglist: any[] = [];
   itemId: any = '';
   userId = localStorage.getItem('userId');
-  stageImg = '';
+  stageImg1 = false;
+  stageImg2 = false;
+  stageImg3 = false;
+  stageImg4 = false;
+  stageImg5 = false;
+  stageImg6 = false;
+  stageImg7 = false;
+  stageImg8 = false;
+  screenWidth = document.body.clientWidth;
+
+  @Watch('this.screenWidth')
+  reloadTable () {
+    const dom: any = document.getElementById('img')
+    console.log(dom.width)
+  }
 
   // 获取所有客户标签
   async mounted () {
+    window.onresize = () => {
+      this.screenWidth = document.body.clientWidth
+    }
     const data = {
       where: {
         and: [{ field: 2200000182035321, query: { in: [2300006607764731] } }]
@@ -49,10 +64,12 @@ export default class Home extends Vue {
       const name = result[i].fields[0].values[0].title
       const value = result[i].fields[1].values[0].value
       const item_id = result[i].item_id
+      const img = result[i].fields[3].values[0].value
       const ob = {
         id: item_id,
         name: name,
         value: value,
+        img: img,
         field: field.customerStage
       }
       this.customStage.push(ob)
@@ -81,17 +98,42 @@ export default class Home extends Vue {
       return
     }
     this.itemId = res[0].item_id
-    // const fields = res[0].fields
-    // for (let i = 0; i < fields.length; i++) {
-    //   if (fields[i].field_id === field.customerStage) {
-    //     const values = fields[i].values
-    //     for (let j = 0; j < values.length; j++) {
-    //       const itemid = values[j].item_id
-    //       //const dom: any = document.getElementById(itemid)
-    //       // dom.className = 'selected'
-    //     }
-    //   }
-    // }
+    const fields = res[0].fields
+    for (let i = 0; i < fields.length; i++) {
+      if (fields[i].field_id === field.customerStage) {
+        const values = fields[i].values
+        for (let j = 0; j < values.length; j++) {
+          const itemid = values[j].item_id
+          this.StageImg(itemid)
+        }
+      }
+    }
+  }
+
+  // 选中图片
+  StageImg (itemid: any) {
+    for (let i = 0; i < this.customStage.length; i++) {
+      const obj = this.customStage[i]
+      if (obj.id === itemid) {
+        if (obj.img === 1) {
+          this.stageImg1 = true
+        } else if (obj.img === 2) {
+          this.stageImg2 = true
+        } else if (obj.img === 3) {
+          this.stageImg3 = true
+        } else if (obj.img === 4) {
+          this.stageImg4 = true
+        } else if (obj.img === 5) {
+          this.stageImg5 = true
+        } else if (obj.img === 6) {
+          this.stageImg6 = true
+        } else if (obj.img === 7) {
+          this.stageImg7 = true
+        } else if (obj.img === 8) {
+          this.stageImg8 = true
+        }
+      }
+    }
   }
 
   // 选择
