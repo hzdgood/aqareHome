9952aaecab060a2b820a28847c2d80e2271dc72f
hzdgood <hzd_163@sina.com>
2022-11-15 11:28:25 +0800

    update code

--- aqaraWeixin/src/config/interFace.ts
+++ aqaraWeixin/src/config/interFace.ts
@@ -1,20 +1,12 @@
 import axios from 'axios'
 import { SignRes } from 'wecom-sidebar-jssdk'
-
 // const httpUrl = 'http://localhost:8081' // 测试url
-const typeUrl = 'http://aqara.club:8080'
 const httpUrl = 'http://aqara.club:8081' // 生产环境
-// const httpUrl = 'http://aqara.club:8091' // 生产环境
 const huobanUrl = 'https://api.huoban.com' // 伙伴云
 
 export const config = {
-  /* 8081 */
   corpId: 'ww9a717b03b06063e3', // 企业ID
   agentId: '1000046' // 应用ID
-
-  /* 8091 */
-  // corpId: 'ww728dd07f7641e567',
-  // agentId: '1000004'
 }
 
 const post = async (url: string, data: object) => {
@@ -94,31 +86,6 @@ export const groupchat = async (chatId: string) => {
   })
   return response.data
 }
-
-// 获取联系人
-// export const externalList = async (userId: string) => {
-//   const response = await axios.request<SignRes>({
-//     method: 'GET',
-//     url: httpUrl + '/wechat/externalList',
-//     params: {
-//       userId: userId,
-//       type: httpUrl
-//     }
-//   })
-//   return response.data
-// }
-// 获取群联系人
-// export const groupList = async (chatId: string) => {
-//   const response = await axios.request<SignRes>({
-//     method: 'GET',
-//     url: httpUrl + '/wechat/grouplist',
-//     params: {
-//       chatId: chatId,
-//       type: httpUrl
-//     }
-//   })
-//   return response.data
-// }
 
 export const userInfo = async () => {
   const url = httpUrl + '/huoban/getTicket'
@@ -260,3 +227,28 @@ export const batchAddPlan = async (tableId: string, data: object) => {
   })
   return response
 }
+
+// 获取联系人
+// export const externalList = async (userId: string) => {
+//   const response = await axios.request<SignRes>({
+//     method: 'GET',
+//     url: httpUrl + '/wechat/externalList',
+//     params: {
+//       userId: userId,
+//       type: httpUrl
+//     }
+//   })
+//   return response.data
+// }
+// 获取群联系人
+// export const groupList = async (chatId: string) => {
+//   const response = await axios.request<SignRes>({
+//     method: 'GET',
+//     url: httpUrl + '/wechat/grouplist',
+//     params: {
+//       chatId: chatId,
+//       type: httpUrl
+//     }
+//   })
+//   return response.data
+// }
