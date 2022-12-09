35996195ed07f649e0a7766eb7d6a0a989a40f19
hzdgood <hzd_163@sina.com>
2022-12-09 18:50:25 +0800

    update code

--- aqaraClub/src/main/java/com/aqara/common/properties/ProgramProperties.java
+++ aqaraClub/src/main/java/com/aqara/common/properties/ProgramProperties.java
@@ -1,2 +1,54 @@
-package com.aqara.common.properties;public class ProgramProperties {
+package com.aqara.common.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "program")
+public class ProgramProperties {
+    private String code2Session;
+    private String phoneNumber;
+    private String accessToken;
+    private String appid;
+    private String secret;
+
+    public String getCode2Session() {
+        return code2Session;
+    }
+
+    public void setCode2Session(String code2Session) {
+        this.code2Session = code2Session;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
 }
