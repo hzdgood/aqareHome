876a1179bdec1527e670b94bee9f10005285a26d
hzdgood <hzd_163@sina.com>
2023-02-09 16:01:24 +0800

    update code

--- aqaraClub/src/main/java/com/aqara/common/interceptor/MyInterceptor.java
+++ aqaraClub/src/main/java/com/aqara/common/interceptor/MyInterceptor.java
@@ -4,19 +4,22 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.*;
+
 @Component
 public class MyInterceptor implements HandlerInterceptor {
     // 进入controller方法之前调用
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String tokens = request.getHeader("tokens");
-        String huobanTicket = request.getHeader("X-Huoban-Ticket");
+        String huoBanTicket = request.getHeader("X-Huoban-Ticket");
         String reqUrl = request.getRequestURI();
         StringUtils StrUtils = new StringUtils();
         if(reqUrl.indexOf("getTokens") != -1){
-            System.out.println("getTokens");
-        } else if(StrUtils.isNotBlank(tokens) || StrUtils.isNotBlank(huobanTicket)){
-            System.out.println("postHandler");
+            System.out.println("getTokens:" + reqUrl);
+        } else if(StrUtils.isNotBlank(tokens) || StrUtils.isNotBlank(huoBanTicket)){
+            System.out.println("postHandler:" + reqUrl);
+        } else {
+            System.out.println("Interceptor:" + reqUrl);
         }
         return true;
     }
