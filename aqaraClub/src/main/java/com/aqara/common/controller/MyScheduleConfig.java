5c34afcd7e3e7a9a95c83732b5f987b4698a1837
hzdgood <hzd_163@sina.com>
2023-02-14 11:16:35 +0800

    update code

--- aqaraClub/src/main/java/com/aqara/common/controller/MyScheduleConfig.java
+++ aqaraClub/src/main/java/com/aqara/common/controller/MyScheduleConfig.java
@@ -90,16 +90,16 @@ public class MyScheduleConfig {
             e.printStackTrace();
         }
     }
-//    @Scheduled(cron = "0 00 10 * * ?")
-//    private void projectBroadcast() {
-//        List<Project> Project = ProjectService.currentData();
-//        String WX_TOKEN = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0adf9002-d736-4921-b35f-f36c6429b097";
-//        String str0 = "**一个月内未跟踪项目** \n";
-//        String str = ProjectService.getCurtainData("徐汇绿地缤纷城店", Project) + "\n";
-//        String str1 = ProjectService.getCurtainData("普陀绿地缤纷城店", Project) + "\n";
-//        String str2 = ProjectService.getCurtainData("虹口太阳宫店", Project) + "\n";
-//        String str3 = ProjectService.getCurtainData("闵行龙之梦店", Project) + "\n";
-//        String res = str0 + str + str1 + str2 + str3;
-//        HttpService.workRequset(res, WX_TOKEN);
-//    }
+    @Scheduled(cron = "0 00 10 * * ?")
+    private void projectBroadcast() {
+        List<Project> Project = ProjectService.currentData();
+        String WX_TOKEN = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0adf9002-d736-4921-b35f-f36c6429b097";
+        String str0 = "**一个月内未跟踪项目** \n";
+        String str = ProjectService.getCurtainData("徐汇绿地缤纷城店", Project) + "\n";
+        String str1 = ProjectService.getCurtainData("普陀绿地缤纷城店", Project) + "\n";
+        String str2 = ProjectService.getCurtainData("虹口太阳宫店", Project) + "\n";
+        String str3 = ProjectService.getCurtainData("闵行龙之梦店", Project) + "\n";
+        String res = str0 + str + str1 + str2 + str3;
+        HttpService.workRequset(res, WX_TOKEN);
+    }
 }
