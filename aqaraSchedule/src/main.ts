5b1713a0df3a91b9f8c97f84bba2d6888903987e
hzdgood <hzd_163@sina.com>
2023-06-21 16:19:24 +0800

    update code

--- aqaraSchedule/src/main.ts
+++ aqaraSchedule/src/main.ts
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Calendar from '@/calendar/index.vue'
 import store from './store'
-import { userInfo, fetchUserId, fetchSignatures, config } from '@/config/interFace'
-import { checkRedirect, initSdk } from 'wecom-sidebar-jssdk'
+import { userInfo } from '@/config/interFace'
 import BaiduMap from 'vue-baidu-map'
 import 'ant-design-vue/dist/antd.css'
 
@@ -12,7 +11,9 @@ Vue.use(BaiduMap, {
 Vue.config.productionTip = false
 localStorage.clear()
 
-const doInfo = async () => {
+doInfo()
+
+async function doInfo () {
   await userInfo().then(function (response) {
     localStorage.setItem('ticket', response.data.ticket)
     new Vue({
@@ -21,7 +22,3 @@ const doInfo = async () => {
     }).$mount('#app')
   })
 }
-
-checkRedirect(config, fetchUserId) // 重定向获取 code（用户身份）
-  .then(() => initSdk(config, fetchSignatures)) // 初始化 JsSdk
-  .then(() => doInfo())
