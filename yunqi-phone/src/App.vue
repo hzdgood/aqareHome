f55507f17e0a8ef734e3e5e5c35cf36dbe96aec0
hzdgood <hzd_163@sina.com>
2023-09-05 15:37:17 +0800

    update code

--- yunqi-phone/src/App.vue
+++ yunqi-phone/src/App.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import LoginView from './views/LoginView.vue';
 </script>
 
 <template>
-  <header>
+  <div>
+    <LoginView></LoginView>
+  </div>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <RouterView /> -->
 </template>
 
 <style scoped>
--- yunqi-phone/src/router/index.ts
+++ yunqi-phone/src/router/index.ts
@@ -10,12 +10,20 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/workSheet',
+      name: 'workSheet',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/WorkSheetView.vue')
+    },
+    {
+      path: '/project',
+      name: 'project',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProjectView.vue')
     }
   ]
 })
