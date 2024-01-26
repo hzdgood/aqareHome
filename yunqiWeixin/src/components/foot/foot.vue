59e917bfdc9b01522e7f47453f45db8c008fa2f2
hzdgood <hzd_163@sina.com>
2024-01-26 15:11:07 +0800

    updat code +1

--- yunqiWeixin/src/components/foot/foot.vue
+++ yunqiWeixin/src/components/foot/foot.vue
@@ -3,11 +3,15 @@
     <div class="footDiv" @click="openOption()">+</div>
     <div class="footContent" v-show="formState.optionStatus">
       <div @click="projectCheck">项目</div>
+      <div @click="followCheck">跟进</div>
       <div @click="schemeCheck">方案</div>
       <div @click="collentCheck">收款</div>
     </div>
     <div v-if="formState.projectShow">
       <project @close="close()"></project>
+    </div>
+    <div v-if="formState.followShow">
+      <follow @close="close()"></follow>
     </div>
     <div v-if="formState.schemeShow">
       <scheme @close="close()"></scheme>
@@ -21,6 +25,7 @@
 <script lang="ts" setup>
 import { reactive } from 'vue';
 import project from '../footSub/project.vue'
+import follow from '../footSub/follow.vue'
 import scheme from '../footSub/scheme.vue'
 import collent from '../footSub/collent.vue'
 
@@ -37,33 +42,50 @@ interface FormState {
   projectShow: boolean
   schemeShow: boolean
   collentShow: boolean
+  followShow: boolean
 }
 
 const formState = reactive<FormState>({
   optionStatus: false,
   projectShow: false,
   schemeShow: false,
-  collentShow: false
+  collentShow: false,
+  followShow: false
 })
 
 const projectCheck = function () {
   formState.projectShow = true
   formState.collentShow = false
   formState.schemeShow = false
+  formState.followShow = false
+}
+
+const followCheck = function(){
+  formState.projectShow = false
+  formState.collentShow = false
+  formState.schemeShow = false
+  formState.followShow = true
 }
 
 const schemeCheck = function () {
-
+  formState.projectShow = false
+  formState.collentShow = false
+  formState.schemeShow = true
+  formState.followShow = false
 }
 
 const collentCheck = function(){
-
+  formState.projectShow = false
+  formState.collentShow = true
+  formState.schemeShow = false
+  formState.followShow = false
 }
 
 const close = function () {
   formState.schemeShow = false
   formState.projectShow = false
   formState.collentShow = false
+  formState.followShow = false
 }
 
 </script>
--- yunqiWeixin/src/components/footSub/scheme.vue
+++ yunqiWeixin/src/components/footSub/scheme.vue
@@ -1,6 +1,15 @@
 <template>
-
+  <div>
+    <div class="buttonPos">
+      <a-button type="primary">提交</a-button>
+      <a-button type="primary" @click="closePage()">关闭</a-button>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
+const emit = defineEmits(['close'])
 
+const closePage = () => {
+  emit('close')
+}
 </script>
